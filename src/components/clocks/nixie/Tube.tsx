6c037b7988af035e6df00ac30caadae9af64ddcd
Tycho-Euler <main.clayton@gmail.com>
2026-04-25 08:57:45 -0600

    Address vercel build issues

--- src/components/clocks/nixie/Tube.tsx
+++ src/components/clocks/nixie/Tube.tsx
@@ -346,7 +346,7 @@ export default function Tube({
       <Posts />
       <Backing />
       <Screen />
-      {/* <Lights displayIndex={displayIndex} /> */}
+      <Lights displayIndex={displayIndex} />
     </group>
   );
 }
